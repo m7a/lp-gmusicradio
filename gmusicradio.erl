63fdd0d164601c378fb46ec8d34a374d5fcdf4df
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2024-01-06 03:01:36 +0100

    Intermediate commit: Implemented most features. Unicode still does not work for whatever reason... Analying...

--- gmusicradio.erl
+++ gmusicradio.erl
@@ -5,58 +5,77 @@
 
 -record(song, {idx, path, description, lastplay, playcount, rating}).
 
-% TODO -- X THIS COULD BE USER CONFIG SETTINGS --
--define(CHAOS_FACTOR, 2.0).
--define(MIN_GOOD_PERC, 30).
--define(DEFAULT_SCHEDULE, 60).
--define(AWAIT_SONG_PLAYBACK, 60000).
-% TODO TESTING ONLY REVERT TO 10 ONCE OK
--define(PODCAST_EVERY, 3).
-% TODO -- END CONFIG SETTINGS --
-
-% TODO would be nice if we were to use the default as
-%      configured in gmusicbrowser
--define(DEFAULT_RATING, 60).
+% TODO ADD COMMENTS TO SECTIONS.
+% TODO CONSIDER ADDING LIFECYCLE FEATURE
+% TODO UNICODE BUG
 
 %---------------------------------------------------------------[ Entrypoint ]--
 main([]) ->
-	% TODO PROCESS PER CONFIG FILE OR SOMETHING
-	database_read("/home/linux-fan/.config/gmusicbrowser/gmbrc"),
-	mainloop(playback_init(schedule_compute(?DEFAULT_SCHEDULE)),
-		1,
-		% TODO MAKE THIS CONFIGURABLE
-		podcast_init("/data/programs/music2/supplementary/news/conf",
-			"/data/programs/music2/supplementary/news/pod")),
+	Conf = main_read_config(),
+	database_read(dict:fetch(gmbrc, Conf),
+					dict:fetch(default_rating, Conf)),
+	main_loop(Conf, playback_init(schedule_compute(Conf)), 1,
+			podcast_init(Conf)),
 	ets:delete(gmusicradio_songs).
 
+main_read_config() ->
+	{ok, UserHome} = init:get_argument(home),
+	ConfFile = filename:join(UserHome, ".mdvl/gmusicradio_config.erl"),
+	% For podcast_dir  and default_rating the values could be parsed from
+	%     podcast_conf and gmbrc respectively. This is more complicated than
+	% stating them explicitly here. Hence, the less-complicated explicit
+	% implementation is provided here.
+	ConfDefault = dict:from_list([
+		{podcast_conf, "/data/programs/music2/supplementary/news/conf"},
+		{podcast_dir,  "/data/programs/music2/supplementary/news/pod"},
+		{gmbrc,        filename:join([UserHome, ".config",
+						"gmusicbrowser", "gmbrc"])},
+		{podcast_chck,   10},
+		{schedule_len,   60},
+		{await_ms,       60000},
+		{chaos_factor,   2.0},
+		{min_good_perc,  30},
+		{default_rating, 60}
+	]),
+	case file:read_file_info(ConfFile) of
+	{ok, _FileInfo} ->
+		{ok, NewConf} = file:consult(ConfFile),
+		dict:map(fun({Key, OldValue}) ->
+				case dict:is_key(Key, NewConf) of
+				true  -> dict:lookup(Key, NewConf);
+				false -> OldValue
+				end
+			end, ConfDefault);
+	{error, _} ->
+		ConfDefault
+	end.
+
 % Endless loop processing
-mainloop({finish, Next, none, _}, Ctr, PodcastState) ->
-	[H|T] = schedule_compute(?DEFAULT_SCHEDULE),
-	mainloop({await, Next, H, T}, Ctr + 1, PodcastState);
-mainloop(PlaybackState={await, _, _, _}, Ctr, PodcastState) ->
-	timer:sleep(?AWAIT_SONG_PLAYBACK),
-	NewPodcastState = case (Ctr rem ?PODCAST_EVERY) == 0 of
+main_loop(Conf, {finish, Next, none, _}, Ctr, PodcastState) ->
+	[H|T] = schedule_compute(Conf),
+	main_loop(Conf, {await, Next, H, T}, Ctr + 1, PodcastState);
+main_loop(Conf, PlaybackState={await, _, _, _}, Ctr, PodcastState) ->
+	timer:sleep(dict:fetch(await_ms, Conf)),
+	NewPodcastState = case (Ctr rem dict:fetch(podcast_chck, Conf)) == 0 of
 			true  -> podcast_process(PodcastState);
 			false -> PodcastState
 			end,
 	StateNew = playback_continue(PlaybackState),
-	mainloop(StateNew, Ctr + 1, NewPodcastState).
-
-% TODO USE Erlang open_port({spawn_executable, BINARY}, [{args, [arg0, arg1, ...]}, {..see link..}])
-% + https://erlang.org/pipermail/erlang-questions/2007-February/025210.html
-% find it in path, then use these primitives to run a subprocess
+	main_loop(Conf, StateNew, Ctr + 1, NewPodcastState).
 
 %------------------------------------------------------------[ GMBRC Parsing ]--
 % Reads the songs from the GMBRC and stores them in ets table `songs` by idx key
 
-database_read(GMBRC) ->
+database_read(GMBRC, DefaultRating) ->
 	io:fwrite("Read GMBRC... "),
 	{ok, RawDataBinary} = file:read_file(GMBRC),
 	{await_eof, Lines, _Keys} = lists:foldl(fun database_line/2,
 			{await_marker, invalid, invalid},
 			binary:split(RawDataBinary, <<"\n">>, [global, trim])),
 	ets:new(gmusicradio_songs, [set, named_table, {keypos, #song.idx}]),
-	lists:foreach(fun database_convert_store/1, Lines),
+	lists:foreach(fun(Line) ->
+				database_convert_store(DefaultRating, Line)
+			end, Lines),
 	io:fwrite("OK~n").
 
 database_line(<<"[Songs]">>, {await_marker, invalid, invalid}) ->
@@ -74,25 +93,23 @@ database_line(CSV, {process_contents, Tail, Keys}) ->
 database_line(_Line, {await_eof, List, Keys}) ->
 	{await_eof, List, Keys}.
 
-database_convert_store(Line) ->
-	Idx = binary_to_integer(database_keyfind(<<"idx">>, Line)),
+database_convert_store(DefaultRating, L) ->
+	Idx = binary_to_integer(database_keyfind(<<"idx">>, L)),
 	ets:insert(gmusicradio_songs, #song{
 		idx=Idx,
-		path=filename:join(database_keyfind(<<"path">>, Line),	
-					database_keyfind(<<"file">>, Line)),
-		description=io_lib:format("~-10ts ~-20ts ~-20ts (~ts)", [
-			% TODO SOME PROBLEM WITH UNICODE ... SOMEWHERE...
-			database_keyfind(<<"artist">>, Line),
-			database_keyfind(<<"album">>,  Line),
-			database_keyfind(<<"title">>,  Line),
-			database_keyfind(<<"year">>,   Line)
+		path=filename:join(database_keyfind(<<"path">>, L),	
+					database_keyfind(<<"file">>, L)),
+		description=io_lib:format("~ts ~ts ~ts (~ts)", [
+			database_utf8p(10, database_keyfind(<<"artist">>, L)),
+			database_utf8p(20, database_keyfind(<<"album">>,  L)),
+			database_utf8p(20, database_keyfind(<<"title">>,  L)),
+			database_utf8p( 4, database_keyfind(<<"year">>,   L))
 		]),
-		lastplay=binary_to_integer(database_keyfind(<<"lastplay">>,
-									Line)),
-		playcount=binary_to_integer(database_keyfind(<<"playcount">>,
-									Line)),
-		rating=case database_keyfind(<<"rating">>, Line) of
-			<<>>  -> ?DEFAULT_RATING;
+		lastplay=binary_to_integer(database_keyfind(<<"lastplay">>, L)),
+		playcount=binary_to_integer(
+					database_keyfind(<<"playcount">>, L)),
+		rating=case database_keyfind(<<"rating">>, L) of
+			<<>>  -> DefaultRating;
 			Value -> binary_to_integer(Value)
 		end
 	}).
@@ -101,9 +118,23 @@ database_keyfind(Key, List) ->
 	{_K, Value} = lists:keyfind(Key, 1, List),
 	Value.
 
-%-----------------------------------------------------[ Schedule Computation ]--
+% TODO THIS UNICODE PART DOES NOT WORK YET...
+database_utf8p(Pad, Binary) ->
+	Primary = unicode:characters_to_binary(bitstring_to_list(Binary), utf8),
+	Len = string:length(Primary),
+	io:fwrite("str=<~p> len=<~p>~n", [Primary, Len]),
+	case Len > Pad of
+	true ->
+		string:slice(Primary, 0, Pad);
+	false ->
+		[Primary|lists:map(fun(_X) -> " " end, lists:seq(1, Pad - Len))]
+	end.
 
-schedule_compute(Limit) ->
+%-----------------------------------------------------[ Schedule Computation ]--
+schedule_compute(Conf) ->
+	MinGoodPerc = dict:fetch(min_good_perc, Conf),
+	ChaosFactor = dict:fetch(chaos_factor,  Conf),
+	ScheduleLen = dict:fetch(schedule_len,  Conf),
 	% unclear why select_count did not return the intended output here?
 	Count1 = length(ets:select(gmusicradio_songs,
 						schedule_construct_match(0))),
@@ -125,17 +156,17 @@ schedule_compute(Limit) ->
 			" 4 Stars ~5w (~5.2f%)~n 5 Stars ~5w (~5.2f%)~n",
 			[Count1, Count2, Perce2, Count3, Perce3,
 			Count4, Perce4, Count5, Perce5]),
-	Duplicate = case Perce4 + Perce5 < ?MIN_GOOD_PERC of
-			true  -> trunc(?MIN_GOOD_PERC / (Perce4 + Perce5));
+	Duplicate = case Perce4 + Perce5 < MinGoodPerc of
+			true  -> trunc(MinGoodPerc / (Perce4 + Perce5));
 			false -> 1
 			end,
 	io:fwrite("Duplicate = ~p~n", [Duplicate]),
 	Schedule = schedule_merge([
-		schedule_shuffle(Group5, Duplicate),
-		schedule_shuffle(Group4, Duplicate),
-		schedule_shuffle(Group3, 1),
-		schedule_shuffle(Group2, 1)
-	], Limit),
+		schedule_shuffle(ChaosFactor, Group5, Duplicate),
+		schedule_shuffle(ChaosFactor, Group4, Duplicate),
+		schedule_shuffle(ChaosFactor, Group3, 1),
+		schedule_shuffle(ChaosFactor, Group2, 1)
+	], ScheduleLen),
 	io:fwrite("Schedule of ~w songs:~n", [length(Schedule)]),
 	lists:foreach(fun(ID) ->
 			[Entry] = ets:lookup(gmusicradio_songs, ID),
@@ -150,11 +181,11 @@ schedule_construct_match(Rating) ->
 				X#song.rating =< (Rating + 20) -> X end).
 
 % https://stackoverflow.com/questions/8817171/shuffling-elements-in-a-list-
-schedule_shuffle(Group, Duplicate) ->
+schedule_shuffle(ChaosFactor, Group, Duplicate) ->
 	lists:flatten(lists:map(fun(_Ctr) ->
 		[Y || {_, Y} <- lists:sort(
 			lists:map(fun(S) ->
-					{rand:uniform() * ?CHAOS_FACTOR +
+					{rand:uniform() * ChaosFactor +
 						S#song.playcount, S#song.idx}
 				end, Group)
 		)]
@@ -185,8 +216,7 @@ schedule_merge_annotated(Schedule, Limit, AnnotatedGroups) ->
 
 %-----------------------------------------------------[ Playback Integration ]--
 playback_init([H1|[H2|ScheduleT]]) ->
-	ok = playback_enqueue(H1),
-	%ok = playback_enqueue(H2), % TODO TEST
+	{ok, _Cnt} = playback_enqueue(H1),
 	{await, H1, H2, ScheduleT}.
 
 playback_continue({await, Await, Next, Sched}) ->
@@ -209,16 +239,17 @@ playback_continue({await, Await, Next, Sched}) ->
 playback_enqueue(ID) ->
 	[Entry] = ets:lookup(gmusicradio_songs, ID),
 	io:fwrite("ENQUEUE ~ts~n", [Entry#song.description]),
-	% TODO NO ESCAPING OR RETURNCODE CHECKING WHATSOEVER
-	os:cmd(io_lib:format("gmusicbrowser -enqueue ~s", [Entry#song.path])),
-	ok.
+	playback_enqueue_file(Entry#song.path).
+
+playback_enqueue_file(File) ->
+	{ok, _Cnt} = subprocess_run_await(["gmusicbrowser", "-enqueue", File]).
 
 playback_check_is_running(ID) ->
 	[Entry] = ets:lookup(gmusicradio_songs, ID),
 	io:fwrite("AWAIT   ~ts~n", [Entry#song.description]),
-	RawStatus = os:cmd("dbus-send --print-reply " ++
-			"--dest=org.gmusicbrowser " ++
-			"/org/gmusicbrowser org.gmusicbrowser.CurrentSong"),
+	{ok, RawStatus} = subprocess_run_await(["dbus-send",
+			"--print-reply", "--dest=org.gmusicbrowser",
+			"/org/gmusicbrowser", "org.gmusicbrowser.CurrentSong"]),
 	% path, file is of interest
 	Lines = lists:map(fun string:trim/1,
 					string:split(RawStatus, "\n", all)),
@@ -230,8 +261,7 @@ playback_check_is_running(ID) ->
 					"absent from output:~n~p~n", [Lines]),
 		error;
 	false ->
-		CMP = binary:list_to_bin(filename:join(Path, File)),
-		CMP == Entry#song.path
+		binary:list_to_bin(filename:join(Path, File)) == Entry#song.path
 	end.
 
 playback_get_dict_entry(_Entry, []) ->
@@ -249,19 +279,47 @@ playback_check_dict_key(Entry, [KeyLine|[ValueLine|Others]]) ->
 	end.
 
 %------------------------------------------------------[ Podcast Interaction ]--
+podcast_init(Conf) ->
+	{Conf, podcast_run_inner(Conf)}.
 
-podcast_init(ConfigDir, OutputDir) ->
-	% TODO SUBPROCESS NO EXIT CODE HANDLING AND NO SPACES SUPPORT
-	os:cmd("podget -d " ++ ConfigDir),
-	{ConfigDir, OutputDir, lists:sort(filelib:wildcard(OutputDir ++ "/**/*.mp3"))}.
+podcast_run_inner(Conf) ->
+	{ok, _Cnt} = subprocess_run_await(["podget", "-d",
+					dict:fetch(podcast_conf, Conf)]),
+	lists:sort(filelib:wildcard(dict:fetch(podcast_dir, Conf) ++
+								"/**/*.mp3")).
 
-podcast_process({ConfigDir, OutputDir, OldState}) ->
-	os:cmd("podget -d " ++ ConfigDir), % TODO SUBPROCESS WARNING
-	NewState = lists:sort(filelib:wildcard(OutputDir ++ "/**/*.mp3")),
+podcast_process({Conf, OldState}) ->
+	NewState = podcast_run_inner(Conf),
 	case NewState -- OldState of
-	[] -> ok; % do nothing
+	[] ->
+		ok; % do nothing
 	NewFiles ->
 		PlayFile = lists:last(NewFiles),
-		io:fwrite("New podcast entries: ~p~n", [PlayFile])
+		io:fwrite("PODCAST ~ts~n", [PlayFile]),
+		playback_enqueue_file(PlayFile)
 	end,
-	{ConfigDir, OutputDir, NewState}.
+	{Conf, NewState}.
+
+%-------------------------------------------------------[ Subprocess Library ]--
+% Enables the spawning of a subprocess with checking its output and return code
+% and using the shell and space safe way of giving the command as a list of
+% parts rather than a shell script...
+
+% https://erlang.org/pipermail/erlang-questions/2007-February/025210.html
+subprocess_run_await([ExecutableName|Args]) ->
+	subprocess_get_data(open_port(
+			{spawn_executable, os:find_executable(ExecutableName)},
+			[{args, Args}, stream, exit_status,
+					use_stdio, stderr_to_stdout, in]
+		), []).
+
+subprocess_get_data(Port, Acc) ->
+	receive
+	{Port, {data, D}} ->
+		subprocess_get_data(Port, [D|Acc]);
+	{Port, {exit_status, RC}} ->
+		case RC == 0 of
+		true  -> {ok, lists:reverse(Acc)};
+		false -> {error, RC, lists:reverse(Acc)}
+		end
+	end.
